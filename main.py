b5b61095063b5647380850bfb50b7ea8c48d663e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-31 17:01:48 +0000

    Comments added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pyqrcode
-def qr_code():
+import pyqrcode#imported the pyqrcode package
+def qr_code():#def is a keyword for cuntion, qr_code is the name of the function
   s='This is POWER python class'
-  d=pyqrcode.create(s)#d is a qr code object
-  d.png('m9y_img.png',scale=6)
+  d=pyqrcode.create(s)#d is a qr code object, here qr code is in object form
+  d.png('my_img.png',scale=6)#converting the qr to an image form #scale is for sixe of image
+  #when you scan the code using camera, you see "his is POWER python class"
   print('Code Executed properly')
+
+if __name__ == '__main__':
+  qr_code()
   
